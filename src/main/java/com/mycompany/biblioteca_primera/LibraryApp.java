fc7bd9897bf12e6c50f093cc84a4df8130ccffd9
IsmaelRQ <151431419+IsmaelRQ@users.noreply.github.com>
2024-11-01 15:26:09 -0500

    Update LibraryApp.java

--- src/main/java/com/mycompany/biblioteca_primera/LibraryApp.java
+++ src/main/java/com/mycompany/biblioteca_primera/LibraryApp.java
@@ -1,58 +1,53 @@
 package com.mycompany.biblioteca_primera;
 
+import javax.swing.JOptionPane;
+
 public class LibraryApp {
 
     public static void main(String[] args) {
         Library library = new Library();
 
-<<<<<<< HEAD
-
+      
         Person student = new Person("123456789", "Juan", "Perez", "Estudiante");
         Person teacher = new Person("987654321", "Paula", "Correa", "Docente");
 
-
         library.registerPerson(student);
+        JOptionPane.showMessageDialog(null, "Persona registrada: " + student);
+        
         library.registerPerson(teacher);
+        JOptionPane.showMessageDialog(null, "Persona registrada: " + teacher);
 
 
         Material book = new Book("PO123", "Polimorfismo", 7);
         Material course = new Course("HE234", "Clases de Herencias", 5);
 
-=======
-        Person student = new Person("123456789", "Juan", "Perez", "Estudiante");
-        Person teacher = new Person("987654321", "Paula", "Correa", "Docente");
-
-        library.registerPerson(student);
-        library.registerPerson(teacher);
-
-        Material book = new Material("PO123", "Polimorfismo", 7);
-        Material course = new Material("HE234", "Clases de Herencias", 5);
->>>>>>> 284040ecbdaa8e54401915380f6c5194f52854fc
-
         library.registerMaterial(book);
+        JOptionPane.showMessageDialog(null, "Material registrado: " + book.getTitle());
+
         library.registerMaterial(course);
+        JOptionPane.showMessageDialog(null, "Material registrado: " + course.getTitle());
 
-<<<<<<< HEAD
-
+    
         library.showMaterials();
 
 
-=======
->>>>>>> 284040ecbdaa8e54401915380f6c5194f52854fc
-        library.loanMaterial("PO123");
-        library.returnMaterial("PO123");
-        library.renewMaterial("PO123");
+        String loanMaterialId = JOptionPane.showInputDialog("Ingrese el ID del material para prestar:");
+        library.loanMaterial(loanMaterialId);
 
-        library.loanMaterial("HE234");
-        library.returnMaterial("HE234");
-        library.renewMaterial("HE234");
-        
-        library.removePerson("123456789");
-        
-<<<<<<< HEAD
+        String returnMaterialId = JOptionPane.showInputDialog("Ingrese el ID del material para devolver:");
+        library.returnMaterial(returnMaterialId);
 
+        String renewMaterialId = JOptionPane.showInputDialog("Ingrese el ID del material para renovar:");
+        library.renewMaterial(renewMaterialId);
+
+        String removePersonId = JOptionPane.showInputDialog("Ingrese la cédula de la persona a eliminar:");
+        library.removePerson(removePersonId);
+        
+ 
+        JOptionPane.showMessageDialog(null, "Materiales actuales en la biblioteca:");
         library.showMaterials();
-=======
->>>>>>> 284040ecbdaa8e54401915380f6c5194f52854fc
+
+
+        library.displayActivityLog();
     }
 }
